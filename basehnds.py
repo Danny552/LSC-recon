331ce9237831d415c73a5ef7e450b49bc0b89a24
Angelica Alvarez <angelica.alvarez@utp.edu.co>
2026-05-17 17:13:41 -0500

    correccion X, E aunque siguen regular

--- basehnds.py
+++ basehnds.py
@@ -4,6 +4,7 @@ import math
 """
 Letters with motion: G, H, J, Ñ, S, Z
 Improvements possible for: R, Q, M, N
+failed: E
 """
 
 # Setup
@@ -44,6 +45,7 @@ while cap.isOpened():
             # Use the distance from the index knuckle to the wrist as a size reference
             hand_scale = dist_2d(lm[5], lm[0]) if dist_2d(lm[5], lm[0]) != 0 else 1.0
 
+            #THUMB
             # 2. Key landmark distances (normalized by hand scale)
             thumb_to_index_tip = dist_2d(lm[4], lm[8]) / hand_scale
             thumb_to_mid_tip = dist_2d(lm[4], lm[12]) / hand_scale
@@ -63,22 +65,25 @@ while cap.isOpened():
             r_tip_to_thumb = dist_2d(lm[16], lm[4]) / hand_scale
             p_tip_to_thumb = dist_2d(lm[20], lm[4]) / hand_scale
 
+            #INDEX
             # distance index to middle
             i_tip_to_mid_tip = dist_2d(lm[8], lm[12]) / hand_scale
+            # distance index to point 2
+            index_to_point_2 = dist_2d(lm[8], lm[2]) / hand_scale
+            # index hook for X (tip close to PIP, not extended)
+            index_tip_to_pip = dist_2d(lm[8], lm[6]) / hand_scale
+            index_tip_to_mcp = dist_2d(lm[8], lm[5]) / hand_scale
+            i_hook = (not i_ext) and index_tip_to_pip < 0.25 and index_tip_to_mcp < 0.55
 
+            #MIDDLE
             # distance middle to index points (7-6)
             mid_to_index_knuckle_6 = dist_2d(lm[9], lm[6]) / hand_scale
             mid_to_index_kuckle_7 = dist_2d(lm[9], lm[7]) / hand_scale
-
-            # index hook for X
-            i_hook = (lm[8].y < lm[6].y) and (not i_ext)
-
-            # distance index to point 2
-            index_to_point_2 = dist_2d(lm[8], lm[2]) / hand_scale
-        
             # distance middle to point 3
             middle_to_point_3 = dist_2d(lm[12], lm[3]) / hand_scale
 
+
+            # RING
             # distance ring 0 - 1
             ring_to_point_1 = dist_2d(lm[16], lm[1]) / hand_scale
             ring_to_point_0 = dist_2d(lm[16], lm[0]) / hand_scale
@@ -96,13 +101,10 @@ while cap.isOpened():
             elif i_ext and m_ext and r_ext and p_ext and thumb_to_point_0 < 0.8:
                 label = "LSC: B"
             
-            # LETTER X: index hook 
-            elif i_hook and not m_ext and not r_ext and not p_ext:
-                label = "LSC: X"
 
-
-            # LETTER D: Only index up
+            # LETTER D: Only index up (make sure index is really extended)
             elif (i_ext and not m_ext and not r_ext and not p_ext and
+            index_tip_to_mcp > 0.65 and index_tip_to_pip > 0.35 and
             (m_tip_to_thumb < 0.4 and r_tip_to_thumb < 0.4 and p_tip_to_thumb < 0.4)):
                 label = "LSC: D"
             
@@ -140,7 +142,6 @@ while cap.isOpened():
             elif p_ext and thumb_to_knuckle_5 > 0.5 and not i_ext and not m_ext and not r_ext:
                 label = "LSC: Y"
 
-
             # LETTER I: Only pinky up
             elif p_ext and not i_ext and not m_ext and not r_ext:
                 label = "LSC: I"
@@ -149,6 +150,11 @@ while cap.isOpened():
             elif (not m_ext and not r_ext and not p_ext and i_ext and
             (thumb_to_knuckle_5 < 0.35 or thumb_to_knuckle_6 < 0.35)):
                 label = "LSC: F"
+
+            # LETTER X: index hook + thumb near middle knuckle 
+            elif (i_hook and not m_ext and not r_ext and not p_ext and
+            thumb_to_knuckle_9 < 0.5 and thumb_to_point_0 > 0.45 and thumb_to_index_tip > 0.25):
+                label = "LSC: X"
 
 
             # --- THE "A" vs "O" vs "C" vs "E" ZONE ---
@@ -171,34 +177,33 @@ while cap.isOpened():
                 if thumb_to_index_tip < 0.25 or thumb_to_mid_tip < 0.2:
                     label = "LSC: O"
                 
-                #LETTER E: all fingers curled
-                elif (i_tip_to_mcp < 0.4 and m_tip_to_mcp < 0.4 and r_tip_to_mcp < 0.4 
-                    and p_tip_to_mcp < 0.35 and (thumb_to_index_tip < 0.4 or thumb_to_mid_tip < 0.4 or
-                    thumb_to_point_0 < 1)):
+                #LETTER E: all fingers curled, thumb tucked across palm
+                elif (i_tip_to_mcp < 0.5 and m_tip_to_mcp < 0.5 and r_tip_to_mcp < 0.5 
+                    and p_tip_to_mcp < 0.45 and thumb_to_point_0 < 0.6 and
+                    thumb_to_knuckle_5 < 0.45):
                     label = "LSC: E"
 
-                # LETTER N: index near point 2 and middle near point 3
-                if (index_to_point_2 < 0.3 and middle_to_point_3 < 0.3):
-                    if (ring_to_point_1 < 0.5 or ring_to_point_0 < 0.5):
+
+                # LETTER N/M: index near point 2 and middle near point 3
+                if (index_to_point_2 < 0.35 and middle_to_point_3 < 0.35):
+                    if (ring_to_point_1 < 0.45 or ring_to_point_0 < 0.45):
                         label = "LSC: M"
                     else:
                         label = "LSC: N"
-
-                
                 
                 # LETTER A: Thumb is very close to point 5 or point 6
-                elif thumb_to_knuckle_5 < 0.35 or thumb_to_knuckle_6 < 0.35:
+                elif (thumb_to_knuckle_5 < 0.35 or thumb_to_knuckle_6 < 0.35) and not i_hook:
                     label = "LSC: A"
                 
                 # LETTER C: Claw (Fingers curved, but a wide gap)
-                elif thumb_to_index_tip > 0.5 and thumb_to_index_tip < 1.2:
+                elif thumb_to_index_tip > 0.45 and thumb_to_index_tip < 0.8:
                     label = "LSC: C"
                 
                 else:
-                    label = "Closed Fist"
+                    label = "LSC: E"
 
             # LETTER L: Index up + Thumb out
-            elif i_ext and (dist_2d(lm[4], lm[5]) / hand_scale) > 0.5 and not m_ext:
+            elif i_ext and thumb_to_knuckle_5 > 0.5 and not m_ext:
                 label = "LSC: L"
 
             
